4e28cf6d10b6ea65f40a49d8308910f72596b912
guillaume <bague.guillaume@live.fr>
2019-11-09 11:54:11 +0100

    Design Creation profile fragment

--- app/src/main/java/com/lab/zicevents/ui/login/CreateProfileFragment.kt
+++ app/src/main/java/com/lab/zicevents/ui/login/CreateProfileFragment.kt
@@ -1,18 +1,22 @@
 package com.lab.zicevents.ui.login
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 
 import com.lab.zicevents.R
+import kotlinx.android.synthetic.main.fragment_create_profile.*
+
 
 /**
  * A simple [Fragment] subclass.
  */
 class CreateProfileFragment : Fragment() {
+
+   private val args: CreateProfileFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -20,5 +24,9 @@ class CreateProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_create_profile, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        create_profile_title.text = "Bonjour, ${args.pseudo}"
+        create_profile_email.setText(args.email)
+    }
 }
