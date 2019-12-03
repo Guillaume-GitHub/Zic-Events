2868c51fda15572999a56bdb04cf115b7fa08081
guillaume <bague.guillaume@live.fr>
2019-12-03 22:24:28 +0100

    Add simple splashsreen with async auth verification

--- app/src/main/java/com/lab/zicevents/LoginActivity.kt
+++ app/src/main/java/com/lab/zicevents/LoginActivity.kt
@@ -17,50 +17,16 @@ import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
     private lateinit var navController : NavController
-    private lateinit var loginViewModel: LoginViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        this.auth = FirebaseAuth.getInstance()
     }
 
     override fun onStart() {
         super.onStart()
-
-        //check if user is sign in
-        if (isUserAuth()){
-            configureViewModel()
-            loginViewModel.getFirestoreUser(auth.currentUser!!.uid)
-            observeFirestoreUserProfile()
-        } else {
-            configureNavController()
-        }
-    }
-
-    /**
-     * Check if an user are already authenticated
-     *@return True = User already authenticated, False = No user authenticate
-     */
-    private fun isUserAuth(): Boolean{
-        return auth.currentUser != null
-    }
-
-    /**
-     * Start MainActivity and finish LoginActivity
-     */
-    private fun launchMainActivity() {
-        startActivity(Intent(this, MainActivity::class.java))
-        this.finish()
-    }
-
-    /**
-     *  Instantiate a LoginViewModel class from LoginViewModelFactory
-     */
-    private fun configureViewModel(){
-        loginViewModel = ViewModelProviders.of(this, LoginViewModelFactory()).get(LoginViewModel::class.java)
+        configureNavController()
     }
 
     /**
@@ -70,17 +36,5 @@ class LoginActivity : AppCompatActivity() {
         navController = findNavController(R.id.login_nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         login_toolbar.setupWithNavController(navController, appBarConfiguration)
-    }
-
-    /**
-     * Observe result of firestore user data fetching operation
-     * Show different views depending to result
-     */
-    private fun observeFirestoreUserProfile(){
-        loginViewModel.profileUserState.observe(this, Observer {userProfile ->
-            if (userProfile.firestoreUser != null) launchMainActivity()
-            else  Toast.makeText(this, "User signed in with no profile into database", Toast.LENGTH_LONG).show()
-            // TODO : REDIRECT TO CREATION PROFILE VIEW OR LOGOUT CURRENT USER
-        })
     }
 }
---
+++ app/src/main/java/com/lab/zicevents/SplashScreenActivity.kt
@@ -0,0 +1,90 @@
+package com.lab.zicevents
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.google.firebase.auth.FirebaseAuth
+import com.lab.zicevents.R
+import com.lab.zicevents.ui.login.LoginViewModel
+import com.lab.zicevents.ui.login.LoginViewModelFactory
+
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var loginViewModel: LoginViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        this.auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("SPLASH CREENN","ACTIVITY")
+
+        //check if user is sign in
+        if (isUserAuth()){
+            configureViewModel()
+            loginViewModel.getFirestoreUser(auth.currentUser!!.uid)
+            observeFirestoreUserProfile()
+        } else {
+            startLoginActivity()
+        }
+    }
+
+    /**
+     * Check if an user are already authenticated
+     *@return True = User already authenticated, False = No user authenticate
+     */
+    private fun isUserAuth(): Boolean{
+        return auth.currentUser != null
+    }
+
+    /**
+     * Start LoginActivity and finish LoginActivity
+     */
+    private fun startLoginActivity() {
+        startActivity(Intent(this, LoginActivity::class.java))
+        this.finish()
+    }
+
+    /**
+     * Start LoginActivity and finish LoginActivity
+     */
+    private fun startMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        this.finish()
+    }
+
+    /**
+     *  Instantiate a LoginViewModel class from LoginViewModelFactory
+     */
+    private fun configureViewModel(){
+        loginViewModel = ViewModelProviders.of(this, LoginViewModelFactory()).get(LoginViewModel::class.java)
+    }
+
+    /**
+     * Observe result of firestore user data fetching operation
+     * Show different views depending to result
+     */
+    private fun observeFirestoreUserProfile(){
+        loginViewModel.profileUserState.observe(this, Observer {userProfile ->
+            if (userProfile.firestoreUser != null) startMainActivity()
+            else  {
+                auth.currentUser!!.delete() // Delete user auth with no profile
+                Toast.makeText(this, "User signed in with no profile into database", Toast.LENGTH_LONG).show()
+                startLoginActivity()
+            }
+        })
+    }
+
+}
