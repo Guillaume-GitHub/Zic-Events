02070c4fceb9ec32f8ad47f18cb0366b045dc023
guillaume <bague.guillaume@live.fr>
2019-12-21 01:33:38 +0100

    Change main toolbar layout + add on destination change listener

--- app/src/main/java/com/lab/zicevents/MainActivity.kt
+++ app/src/main/java/com/lab/zicevents/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.lab.zicevents
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.ActionBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -21,10 +26,19 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.bottom_navigation_deal, R.id.bottom_navigation_event, R.id.bottom_navigation_deal_management , R.id.bottom_navigation_chat, R.id.bottom_navigation_account
+                R.id.bottom_navigation_deal, R.id.bottom_navigation_event, R.id.bottom_navigation_deal_management , R.id.bottom_navigation_chat, R.id.bottom_navigation_profile
             )
         )
+
         main_toolbar.setupWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener{ controller, destination, arguments ->
+            when(destination.id) {
+                R.id.bottom_navigation_profile ->
+                    if (main_toolbar.isVisible) main_toolbar.visibility = View.GONE
+                else ->  if (!main_toolbar.isVisible) main_toolbar.visibility = View.VISIBLE
+            }
+        }
     }
 }
