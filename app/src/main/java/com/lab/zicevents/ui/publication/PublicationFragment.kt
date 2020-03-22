c48ece8a210b69d0bf6d93d544797faee461c35f
guillaume <bague.guillaume@live.fr>
2020-03-22 23:35:42 +0100

    Hide not used menu

--- app/src/main/java/com/lab/zicevents/ui/publication/PublicationFragment.kt
+++ app/src/main/java/com/lab/zicevents/ui/publication/PublicationFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -96,7 +97,6 @@ class PublicationFragment : Fragment(), OnActivityFabClickListener, OnPublicatio
      * Get all publications of user 's subscriptions
      */
     private fun getPublications() {
-        Log.d("FETCH PUBLICATION", "---------> ${this}")
         val user = sharedViewModel.getAuthUser()
         val firebaseUser = FirebaseAuth.getInstance().currentUser
 
